f587ba906a4591ea7894f1af261f0e1116d5ce5f
JX Wang <wjianxu@gmail.com>
2022-12-29 19:06:33 +0800

    fix: 修正 zsh 文件路径

--- zsh/.zshrc-extra.zsh
+++ zsh/.zshrc-extra.zsh
@@ -1,6 +1,6 @@
 DOTFILES_ROOT=$(dirname $(dirname $(readlink -n ~/.zshrc-extra.zsh)))
 
-source $DOTFILES_ROOT/aliases.zsh
+source $DOTFILES_ROOT/zsh/aliases.zsh
 
-source $DOTFILES_ROOT/fzf.zsh
+source $DOTFILES_ROOT/zsh/fzf.zsh
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
