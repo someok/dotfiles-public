f256a1f06e3349125efce2a9f4aef0068e2e6902
JX Wang <wjianxu@gmail.com>
2022-12-29 22:25:46 +0800

    fix: 修正 DOTFILES_ROOT

--- zsh/.zshrc-extra.zsh
+++ zsh/.zshrc-extra.zsh
@@ -1,6 +1,6 @@
 # 建议将 dotfiles-public 放置到 /opt 下，以便所有用户均可访问
 # DOTFILES_ROOT=$(dirname $(dirname $(readlink -n ~/.zshrc-extra.zsh)))
-DOTFILES_ROOT=/opt/dotfiles-public/zsh/.zshrc-extra.zsh
+DOTFILES_ROOT=/opt/dotfiles-public
 
 source $DOTFILES_ROOT/zsh/aliases.zsh
 
