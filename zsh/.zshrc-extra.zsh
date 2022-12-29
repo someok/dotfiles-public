a05610b86da57ac9d420c398c8d8d2dca9a94c56
JX Wang <wjianxu@gmail.com>
2022-12-29 22:19:31 +0800

    feat: 调整 linux 下位置到 /opt

--- zsh/.zshrc-extra.zsh
+++ zsh/.zshrc-extra.zsh
@@ -1,6 +1,8 @@
-DOTFILES_ROOT=$(dirname $(dirname $(readlink -n ~/.zshrc-extra.zsh)))
+# 建议将 dotfiles-public 放置到 /opt 下，以便所有用户均可访问
+# DOTFILES_ROOT=$(dirname $(dirname $(readlink -n ~/.zshrc-extra.zsh)))
+DOTFILES_ROOT=/opt/dotfiles-public/zsh/.zshrc-extra.zsh
 
 source $DOTFILES_ROOT/zsh/aliases.zsh
 
 source $DOTFILES_ROOT/zsh/fzf.zsh
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+[ -f $DOTFILES_ROOT/zsh/.fzf.zsh ] && source $DOTFILES_ROOT/zsh/.fzf.zsh
