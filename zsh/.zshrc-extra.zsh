c30c48d0d372d9d42b02abe07e5c63c39d19ed2c
JX Wang <wjianxu@gmail.com>
2023-01-12 20:23:39 +0800

    feat: 换用更通用的根目录方法

--- zsh/.zshrc-extra.zsh
+++ zsh/.zshrc-extra.zsh
@@ -1,6 +1,7 @@
 # 建议将 dotfiles-public 放置到 /opt 下，以便所有用户均可访问
 # DOTFILES_ROOT=$(dirname $(dirname $(readlink -n ~/.zshrc-extra.zsh)))
-DOTFILES_ROOT=/opt/dotfiles-public
+# DOTFILES_ROOT=/opt/dotfiles-public
+DOTFILES_ROOT=$(dirname ${0:A:h})
 
 source $DOTFILES_ROOT/zsh/aliases.zsh
 
